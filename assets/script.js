78a93997db5d57a1ce94bdc2301663a8bb02d0a0
StunnaDawg <jonsonallen9@gmail.com>
2023-02-21 14:55:57 -0400

    weather data display on page

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', function(){
 
 const key = '1b452da07c9aa9a60476c29a53bc96e0'
 
@@ -11,14 +11,33 @@ function weatherForecast(city) {
     })
     .then(function(data) {
         console.log('--->'+(JSON.stringify(data)));
-        drawWeather(data);
+        
+        const tempData = data.list[0].main.temp;
+        const forcastDate = data.list[0].dt_txt;
+        const windData = data.list[0].wind.speed;
+        const  humidData = data.list[0].main.humidity;
+
+        console.log(tempData, forcastDate, windData, humidData);
+        showWeatherData(data);
     })
     .catch(function() {
         // catch any errors
     });
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+function showWeatherData(data) {
+    const date = document.querySelector('.date');
+    const temp = document.querySelector('.temp');
+    const wind = document.querySelector('.wind');
+    const humid = document.querySelector('.humid');
+
+    date.textContent = data.list[0].dt_txt;
+    temp.textContent = data.list[0].main.temp;
+    wind.textContent = data.list[0].wind.speed;
+    humid.textContent = data.list[0].main.humidity;
+
+}
+
 
 const btnSearch = document.querySelector('#search-btn');
 
@@ -26,6 +45,6 @@ const userInput = document.querySelector('#userInput');
 btnSearch.addEventListener("click", function() {
     console.log('meow')
     console.log(userInput.value);
-    weatherForecast(userInput);
+    weatherForecast(userInput.value);
 })
 })
